64768bd48d0c6ed1c0dbb9e8cc070c1f7be3ed70
Adriano <adriano.selaviles@gmail.com>
2020-06-02 22:45:03 -0700

    latest

--- api/main.go
+++ api/main.go
@@ -1,12 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
 
 var (
 	// injected at build-time
 	version string
 )
 
+func roomHandler(w http.ResponseWriter, r *http.Request) {
+	// FIXME: add check origin to upgrader
+	upgrader := websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024}
+	wsConn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		w.Write([]byte(fmt.Sprintf("%s", err)))
+		return
+	}
+
+	// get room reference
+	// subscribe websocket conn to room state
+	wsConn.WriteMessage(websocket.TextMessage, []byte("connected"))
+	for {
+	}
+}
+
 func main() {
-	fmt.Println(version)
+	rtr := mux.NewRouter()
+	rtr.Methods(http.MethodGet).Path("/room/{room_id}").HandlerFunc(roomHandler)
+
+	if err := http.ListenAndServe(":80", rtr); err != nil {
+		log.Fatal(err)
+	}
 }
