157ac3273b3cfe2ea5eb632daa6efe42e5e3f49e
Adriano <adriano.selaviles@gmail.com>
2020-06-08 22:23:14 -0700

    cors middleware

--- api/ctrl/cors.go
+++ api/ctrl/cors.go
@@ -9,21 +9,27 @@ import (
 func cors(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", strings.Join([]string{
-			http.MethodPost,
-			http.MethodGet,
-			http.MethodOptions,
-			http.MethodPut,
-			http.MethodDelete,
-		}, ","))
-		w.Header().Set("Access-Control-Allow-Headers", strings.Join([]string{
-			"Accept",
-			"Content-Type",
-			"Content-Length",
-			"Accept-Encoding",
-			"X-CSRF-Token",
-			"Authorization",
-		}, ","))
+		w.Header().Set("Access-Control-Allow-Methods",
+			strings.Join([]string{
+				http.MethodPost,
+				http.MethodGet,
+				http.MethodOptions,
+				http.MethodPut,
+				http.MethodDelete,
+			}, ","))
+		w.Header().Set("Access-Control-Allow-Headers",
+			strings.Join([]string{
+				"Accept",
+				"Content-Type",
+				"Content-Length",
+				"Accept-Encoding",
+				"X-CSRF-Token",
+				"Authorization",
+			}, ","))
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 		h.ServeHTTP(w, r)
 	})
 }
