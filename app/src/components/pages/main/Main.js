60cbd5abe1e9e57af3311d3a75812e007f10cc87
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2020-06-08 22:50:31 -0700

    pass real token (#16)

--- app/src/components/pages/main/Main.js
+++ app/src/components/pages/main/Main.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import GameBoard from '../../game/GameBoard';
 import {openWebSocket, login} from '../../../service/api';
+import {getToken} from '../../../helpers/storage'
 
 class Main extends React.Component {
     async componentWillMount() {
-        await login('MOCK_USERNAME', 'MOCK_PASS')
-        // TODO: if login succeeds, we dont need to pass MOCK_TOKEN below
-        openWebSocket('MOCK_GAME_ID', "MOCK_TOKEN")
+        await login('MOCK_USERNAME', 'MOCK_PASS');
+        openWebSocket('MOCK_GAME_ID', getToken());
     }
 
     render() {
