e3f19e8c7108ef98497034218814eabd3de49f32
Felipe Ballesteros <felipe@codecov.io>
2020-06-08 23:52:38 -0500

    mock user and pass

--- app/src/components/pages/main/Main.js
+++ app/src/components/pages/main/Main.js
@@ -4,7 +4,7 @@ import {openWebSocket, login} from '../../../service/api';
 
 class Main extends React.Component {
     async componentWillMount() {
-        await login('felipe', 'pass')
+        await login('MOCK_USERNAME', 'MOCK_PASS')
         // TODO: if login succeeds, we dont need to pass MOCK_TOKEN below
         openWebSocket('MOCK_GAME_ID', "MOCK_TOKEN")
     }
