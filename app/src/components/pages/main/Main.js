4e33290c0684c65d461db2e4f0fba75b6abed53d
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2020-06-08 20:22:12 -0700

    authenticated ws example (#13)

--- app/src/components/pages/main/Main.js
+++ app/src/components/pages/main/Main.js
@@ -2,19 +2,27 @@ import React from 'react'
 import { w3cwebsocket as ws } from "websocket";
 import GameBoard from '../../game/GameBoard';
 
-const client = new ws('ws://localhost:8080/game/MOCK_ID/watch');
-
 class Main extends React.Component {
     componentWillMount() {
-	client.onopen = () => {
-	    console.log('websocket client connected');
-	};
+	    const url = 'ws://localhost:8080'; // FIXME
+	    const game_id = 'MOCK_GAME_ID'; // FIXME
+	    const token = 'MOCK_TOKEN'; // FIXME: replace with token from HTTP GET /login (basic auth with no password)
 
-	// for now simply log the message
-	// to test websocket server conn.
-	client.onmessage = (message) => {
-	    console.log(message.data);
-	};
+	    const wsURL = `${url}/game/${game_id}/watch?token=${token}`;
+
+	    const ws = new WebSocket(wsURL);
+
+	    ws.onopen = () => {
+	        console.log('connected');
+	    };
+
+	    ws.onmessage = (msg) => {
+	        console.log(msg.data); // FIXME: what we sending -- state or moves?
+	    };
+
+	    ws.onclose = () => {
+            console.log('disconnected');
+	    };
     }
 
     render() {
