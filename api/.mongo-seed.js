2114730786b97ec2a5dce441c88f1b184e713fa7
Adriano <adriano.selaviles@gmail.com>
2020-06-11 13:07:50 -0700

    replica set mongodb

--- api/.mongo-seed.js
+++ api/.mongo-seed.js
@@ -1,12 +1,21 @@
-db.createUser(
-  {
-    user: "mock-user",
-    pwd: "mock-pwd",
-    roles: [
-      {
-	      role: "readWrite",
-	      db: "mock-db"
-      }
-    ]
-  }
-)
+async function init() {
+  rs.initiate();
+  rs.status();
+
+  await new Promise(r => setTimeout(r, 2000));
+
+  db.createUser(
+    {
+      user: "mock-user",
+      pwd: "mock-pwd",
+      roles: [
+        {
+  	      role: "readWrite",
+  	      db: "mock-db"
+        }
+      ]
+    }
+  );
+}
+
+init();
